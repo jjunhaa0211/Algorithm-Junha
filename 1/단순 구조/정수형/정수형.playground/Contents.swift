09e4a181e2308ed10255828aa1c6494388819c17
박준하 <goodjunha@gmail.com>
2024-04-28 19:43:19 +0900

    커밋

---
+++ 1/단순 구조/정수형/정수형.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+var number: Int = 1
+number = 2
+number = -1
+number = 10 * 10
+number = 3 / 3
+
+
+print(number)
+
+var number2: Int32 = 32767
+print(number2)
+
+var number3: Int32 = 32780
+print(number2)
+
+
+var number4: Int8 = 32780
+print(number2)
+
+
+
+var number5: UInt = 1
+var number6: UInt = -1
+var number7: UInt = 1 + 1
+var number8: UInt = 1 - 2
