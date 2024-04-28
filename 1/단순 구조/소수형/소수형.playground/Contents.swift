342366ab1f077628e84efc8ab6ad1bacf15447e1
박준하 <goodjunha@gmail.com>
2024-04-28 20:08:22 +0900

    커밋

---
+++ 1/단순 구조/소수형/소수형.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var nf1: Float = 3.14
+var nb1: Double = -3.14
+
+var nf2: Float = 3.12345678901234567890
+var nd2: Float = 3.123456789012345678901234567890
+
+print(nf2)
+print(nd2)
